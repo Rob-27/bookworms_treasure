54cffbe75102ad58c9442a23bb04fc35b7be16ca
Rob-27 <schroederrobin2001@gmail.com>
2026-01-04 19:42:28 +0100

    UI_Button_fixed
    (Removed Keyboard from devices in Input System)

--- bookworms_treasure/Assets/Scripts/UIController.cs
+++ bookworms_treasure/Assets/Scripts/UIController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIController : MonoBehaviour
 {
@@ -20,6 +21,7 @@ public class UIController : MonoBehaviour
 
     public void OpenInventory()
     {
+       
         if (inventoryMenu.activeSelf == true)
         {
             inventoryMenu.SetActive(false);
