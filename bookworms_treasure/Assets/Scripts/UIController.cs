0f9667864798064adc7285dcfdab299a1944941b
Simon-O91 <simon_opel_91@web.de>
2026-01-04 14:57:15 +0100

    Inventory Button Test

---
+++ bookworms_treasure/Assets/Scripts/UIController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    public GameObject inventoryMenu;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OpenInventory()
+    {
+        if (inventoryMenu.activeSelf == true)
+        {
+            inventoryMenu.SetActive(false);
+        }
+
+        else if (inventoryMenu.activeSelf == false)
+        {
+            inventoryMenu.SetActive(true);
+        }
+    }
+}
