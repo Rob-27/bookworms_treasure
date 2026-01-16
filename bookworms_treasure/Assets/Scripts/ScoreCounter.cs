9c355c8eccbbcafee6518b7a9dcd63ee778a3a17
Simon-O91 <simon_opel_91@web.de>
2026-01-16 14:52:38 +0100

    Punktezähler + anderes

---
+++ bookworms_treasure/Assets/Scripts/ScoreCounter.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using TMPro;
+
+public class ScoreCounter : MonoBehaviour
+{
+    public static int scoredPoints = 0;
+
+    private static TMP_Text scoreText;
+
+    void Start()
+    {
+        scoreText = gameObject.GetComponent<TMP_Text>();
+    }
+
+    public static void AddPoint()
+    {
+        scoredPoints++;
+
+        if (scoreText != null && scoredPoints != 10)
+        {
+            scoreText.text = scoredPoints.ToString();
+        }
+
+        else if (scoredPoints == 9)
+        {
+            scoreText.text = "8";
+        }
+    }
+
+    public static void ResetScore()
+    {
+        scoredPoints = 0;
+    }
+}
